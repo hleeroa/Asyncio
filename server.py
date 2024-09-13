e7a09ebfe365450bea7910f792da7fd94a5da2e1
Gera Kalashnikova <153171612+hleeroa@users.noreply.github.com>
2024-09-13 15:26:08 +0500

    Added_url_to_string_transformation.py

--- server.py
+++ server.py
@@ -8,10 +8,39 @@ from models import SessionDB, SwapiPeople, init_orm
 
 MAX_REQUESTS = 5
 
+async def urls_to_str(person_dict: dict, row_names: list, session):
+    for row_name in row_names:
+        values = person_dict[row_name]
+        if row_name == "homeworld":
+            str_values = await get_homeland(values, session)
+            person_dict[row_name] = str_values
+            continue
+        str_values = []
+        for url in values:
+            response = await session.get(url)
+            json_data = await response.json()
+            if row_name == "films":
+                str_values.append(json_data["title"])
+            else:
+                str_values.append(json_data["name"])
+        person_dict[row_name] = str_values
+    return person_dict
+
+async def get_homeland(value, session):
+    response = await session.get(value)
+    json_data = await response.json()
+    return json_data["name"]
+
+def check_if_detail(person_dict):
+    if person_dict == {'detail': 'Not found'}:
+        return True
 
 async def get_person(person_id, session):
     response = await session.get(f"https://swapi.dev/api/people/{person_id}/")
-    json_data = await response.json()
+    json_data_to_modify = await response.json()
+    if check_if_detail(json_data_to_modify):
+        return 
+    json_data = await urls_to_str(json_data_to_modify, ["films", "species", "starships", "vehicles", "homeworld"], session)
     json_data.pop("created", None)
     json_data.pop("edited", None)
     json_data.pop("url", None)
@@ -41,6 +70,11 @@ async def main():
         main_task = asyncio.current_task()
         tasks.remove(main_task)
         await asyncio.gather(*tasks)
+
+
+start = datetime.datetime.now()
+asyncio.run(main())
+print(datetime.datetime.now() - start)
 
 
 start = datetime.datetime.now()
